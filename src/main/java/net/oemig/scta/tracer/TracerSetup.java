089e1b9caa1fb1644a566b64015ea0ad7270d103
Christoph Oemig <christoph.oemig@oemig.net>
2014-03-29 21:51:03 +0100

    parameter for rmiregistry host added

--- src/main/java/net/oemig/scta/tracer/TracerSetup.java
+++ src/main/java/net/oemig/scta/tracer/TracerSetup.java
@@ -15,6 +15,7 @@ import net.oemig.scta.tracer.exception.TracerException;
 public class TracerSetup {
 
 	private static final String TRACER_LOOKUP_NAME = "TracerLookupName";
+	private static final String RMI_REGISTRY_HOST="RmiRegistryHost";
 	public static final String TRACER_VERSION = "2.0";
 
 	public static void main(String[] args) throws Exception {
@@ -61,7 +62,7 @@ public class TracerSetup {
 
 	private ITracerMediator lookupMediator() throws TracerException {
 		try {
-			return (ITracerMediator) LocateRegistry.getRegistry().lookup(
+			return (ITracerMediator) LocateRegistry.getRegistry(System.getProperty(RMI_REGISTRY_HOST)).lookup(
 					System.getProperty(TRACER_LOOKUP_NAME));
 		} catch (AccessException e1) {
 			throw new TracerException(TracerException.ACCESS_EXCEPTION, e1);
@@ -76,7 +77,7 @@ public class TracerSetup {
 		try {
 			ITracerMediator stub = (ITracerMediator) UnicastRemoteObject
 					.exportObject(mediator, 0);
-			LocateRegistry.getRegistry().bind(System.getProperty(TRACER_LOOKUP_NAME), stub);
+			LocateRegistry.getRegistry(System.getProperty(RMI_REGISTRY_HOST)).bind(System.getProperty(TRACER_LOOKUP_NAME), stub);
 		} catch (RemoteException e) {
 			throw new TracerException(TracerException.REMOTE_EXCEPTION, e);
 		} catch (AlreadyBoundException e) {
